54e1fb68bc43e0e70527663de1c96d3021fd2687
Craig Wayne <craigiswayne@gmail.com>
2026-01-05 12:28:02 +0000

    More options for Avatar Badge
    * Birthday
    * Do Not Disturb
    * Add Action
    * Selected Avatar

--- projects/component-library/src/components/avatar-status/avatar-status.stories.ts
+++ projects/component-library/src/components/avatar-status/avatar-status.stories.ts
@@ -1,6 +1,7 @@
 import {Meta, moduleMetadata, StoryObj} from '@storybook/angular';
 import {AvatarComponent, AvatarStatusComponent} from 'component-library';
 import {tailwind_sizes_values} from 'component-library/constants/tailwind-sizes';
+import {tailwind_sizes} from 'component-library/enums/tailwind-sizes.enum';
 
 const meta: Meta<AvatarStatusComponent> = {
   component: AvatarStatusComponent,
@@ -11,7 +12,7 @@ const meta: Meta<AvatarStatusComponent> = {
       ]
     })
   ],
-  tags: ['autodocs', 'avatar'],
+  tags: ['autodocs', 'avatar', 'status', 'presence'],
   argTypes: {
     size: {
       options: tailwind_sizes_values,
@@ -105,5 +106,141 @@ export const AllStatusesWithTextOnly: Story = {
         </lib-avatar-status>
       `
     }
+  }
+};
+
+export const DoNotDisturb: Story = {
+  render: (args) => {
+    const inputs = [
+      args.size ? `[size]="'${args.size}'"` : '',
+      args.status ? `[status]="'${args.status}'"` : ''
+    ].join(' ')
+    return {
+      template: `
+        <lib-avatar-status ${inputs}>
+          <lib-avatar name="Craig Wayne" image_url="https://mockmind-api.uifaces.co/content/human/1.jpg" />
+          <svg ngProjectAs="badge" viewBox="0 -960 960 960" fill="currentColor">
+            <path d="M200-440v-80h560v80H200Z"/>
+          </svg>
+        </lib-avatar-status>
+      `
+    }
+  },
+  args: {
+    status: 'busy',
+    size: tailwind_sizes['4xl']
+  }
+};
+
+export const Birthday: Story = {
+  render: (args) => {
+    const inputs = [
+      args.size ? `[size]="'${args.size}'"` : '',
+      args.status ? `[status]="'${args.status}'"` : ''
+    ].join(' ')
+    return {
+      template: `
+        <lib-avatar-status ${inputs} style="--status-color: white">
+          <lib-avatar name="Craig Wayne" image_url="https://mockmind-api.uifaces.co/content/human/1.jpg" />
+          <img ngProjectAs="badge" src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f382/emoji.svg" style="box-shadow: 0 1px 2px rgba(60, 64, 67, .3), 0 1px 3px 1px rgba(60, 64, 67, .15);"/>
+        </lib-avatar-status>
+      `
+    }
+  },
+  args: {
+    status: 'busy',
+    size: tailwind_sizes['4xl']
+  }
+};
+
+export const AddAction: Story = {
+  render: (args) => {
+    const inputs = [
+      args.size ? `[size]="'${args.size}'"` : '',
+      args.status ? `[status]="'${args.status}'"` : ''
+    ].join(' ')
+    return {
+      template: `
+        <lib-avatar-status ${inputs}>
+          <lib-avatar name="Craig Wayne" image_url="https://mockmind-api.uifaces.co/content/human/1.jpg" />
+          <svg ngProjectAs="badge" viewBox="0 -960 960 960" fill="currentColor">
+            <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
+          </svg>
+        </lib-avatar-status>
+      `
+    }
+  },
+  args: {
+    status: 'online',
+    size: tailwind_sizes['4xl']
+  }
+};
+
+export const SelectedBlue: Story = {
+  render: (args) => {
+    const inputs = [
+      args.size ? `[size]="'${args.size}'"` : '',
+      args.status ? `[status]="'${args.status}'"` : ''
+    ].join(' ')
+    return {
+      template: `
+        <lib-avatar-status ${inputs} style="--status-color: #a1bdf0; --border-color: #a1bdf0">
+          <lib-avatar name="Craig Wayne" image_url="https://mockmind-api.uifaces.co/content/human/1.jpg" [bordered]="true"/>
+          <svg ngProjectAs="badge" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+            <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
+          </svg>
+        </lib-avatar-status>
+      `
+    }
+  },
+  args: {
+    status: 'online',
+    size: tailwind_sizes['4xl']
+  }
+};
+
+export const SelectedDarkBlue: Story = {
+  render: (args) => {
+    const inputs = [
+      args.size ? `[size]="'${args.size}'"` : '',
+      args.status ? `[status]="'${args.status}'"` : ''
+    ].join(' ')
+    return {
+      template: `
+        <lib-avatar-status ${inputs} style="--status-color: rgb(0, 109, 234); --border-color: rgb(0, 109, 234)">
+          <lib-avatar name="Craig Wayne" image_url="https://mockmind-api.uifaces.co/content/human/1.jpg" [bordered]="true"/>
+          <svg ngProjectAs="badge" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+            <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
+          </svg>
+        </lib-avatar-status>
+      `
+    }
+  },
+  args: {
+    status: 'online',
+    size: tailwind_sizes['4xl']
+  }
+};
+
+export const SelectedDarkBlueAlternate: Story = {
+  render: (args) => {
+    const inputs = [
+      args.size ? `[size]="'${args.size}'"` : '',
+      args.status ? `[status]="'${args.status}'"` : ''
+    ].join(' ')
+    return {
+      template: `
+        <lib-avatar-status ${inputs} style="--status-color: rgb(0, 109, 234); --border-color: rgb(0, 109, 234)">
+          <lib-avatar name="Craig Wayne" image_url="https://mockmind-api.uifaces.co/content/human/1.jpg" [bordered]="true"/>
+          <svg ngProjectAs="badge" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor" style="border-color: white">
+            <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
+          </svg>
+        </lib-avatar-status>
+      `
+    }
+  },
+  args: {
+    status: 'online',
+    size: tailwind_sizes['4xl']
   }
 };
